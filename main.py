64fddd6d9d85c7a4036883bd898c6f3407459b95
mathias <mathiasnextkbh@gmail.com>
2022-11-28 13:19:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def isPrime(n):
+    if n < 2: return False
+    if n < 4: return True
+    if n % 2 == 0: return False
+    for i in range(3, 1+int(n**0.5), 2):
+        if n % i == 0: return False
+    return True
